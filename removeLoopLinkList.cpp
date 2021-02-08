00d7c7d2fbb7f54c33f9101d14771839a81e585e
LALIT K <lalitkumar.iiit@gmail.com>
2021-02-08 17:27:36 +0530

    detect and remove linklist Loop

---
+++ removeLoopLinkList.cpp
@@ -0,0 +1,140 @@
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+void removeLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+void removeLoop(Node* head)
+{
+    // code here
+    // just remove the loop without losing any nodes
+    Node *fast,*slow;
+    fast=slow=head;
+    while(fast and slow and fast->next) {
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast) {
+            Node *ptr1=head;
+            Node *loopAt=slow;
+            if(loopAt==head) {
+                while(loopAt->next!=head)
+                    loopAt=loopAt->next;
+            }
+            else {
+                while(ptr1->next!=loopAt->next) {
+                ptr1=ptr1->next;
+                loopAt=loopAt->next;
+                //if(ptr1->next==loopAt->next)
+                  //  break;
+            }
+            }
+            //cout<<"First element is "<<loopAt->data<<endl;;
+            loopAt->next=NULL;
+            break;
+        }
+    }
+}
