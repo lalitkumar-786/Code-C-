df4931baaaafdd7bae7921fd6cfaf4a3173dfc0a
LALIT K <lalitkumar.iiit@gmail.com>
2021-02-23 19:08:43 +0530

    Kth smallest element using priority Queue

---
+++ KthSmallestElement.cpp
@@ -0,0 +1,45 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(int *, int, int, int);
+ 
+int main()
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        cout<<kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+
+// arr : given array
+// l : starting index of the array i.e 0
+// r : ending index of the array i.e size-1
+// k : find kth smallest element and return using this function
+int kthSmallest(int arr[], int l, int r, int k) {
+    //code here
+    priority_queue<int,vector<int>,less<int>> maxHeap;
+    for(int i=0;i<=r;i++){
+        maxHeap.push(arr[i]);
+        if(maxHeap.size()>k)
+            maxHeap.pop();
+    }
+    return maxHeap.top();
+}
