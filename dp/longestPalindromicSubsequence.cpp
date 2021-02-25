17da62bc790ddccee85aed55ab650522ded5f90b
LALIT K <lalitkumar.iiit@gmail.com>
2021-02-25 12:55:21 +0530

    Longest palindromic subsequence leetcode

---
+++ dp/longestPalindromicSubsequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int LPS(string s1,string s2){
+        int l1=s1.length();
+        int l2=s2.length();
+        
+        int dp[l1+1][l2+1];
+        for(int i=0;i<=l1;i++){
+            for(int j=0;j<=l2;j++){
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+            }
+        }
+        
+        for(int i=1;i<=l1;i++){
+            for(int j=1;j<=l2;j++)
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+        return dp[l1][l2];
+    }
+    int longestPalindromeSubseq(string s) {
+        int l=s.length();
+        if(l==1)
+            return 1;
+        else{
+            string s2=s;
+            reverse(s2.begin(),s2.end());
+            return LPS(s,s2);  
+        }
+    }
+};
