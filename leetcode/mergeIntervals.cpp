4392f9656713804b72df515f20fa2c30e7797903
LALIT K <lalitkumar.iiit@gmail.com>
2021-05-17 16:36:16 +0530

    MERGE intervals using vector

---
+++ leetcode/mergeIntervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        
+        if(intervals.size()==1)
+            return intervals;
+     
+        res.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            
+            if(intervals[i][0]<=res.back()[1]){
+                res.back()[1]= max(res.back()[1],intervals[i][1]);
+            }
+            else{
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+    }
+};
