341f6d52195e957c5b0d542053f71a9cb873913d
LALIT K <lalitkumar.iiit@gmail.com>
2021-05-19 11:53:35 +0530

    Update getSubsets.cpp

--- getSubsets.cpp
+++ getSubsets.cpp
@@ -1,61 +1,27 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
-void displaySubsets(vector<vector<int>> v){
-	for(int i=0;i<v.size();i++){
-		for(auto it=v[i].begin();it!=v[i].end();it++){
-			cout<<*it<<" ";
-		}
-		cout<<endl;
-	}
-}
-
-vector<vector<int>> getSubsets_itr(int n,vector<vector<int>> &v) {
-	if(n>0){
-		v.push_back({});
-		v.push_back({1});
-	}
-
-	for(int num=2;num<=n;num++){
-		int l=v.size();
-		for(int i=0;i<l;i++){
-		//	cout<<"Adding partitions... for "<<i<<endl;
-			vector<int> r=v[i];
-			r.push_back(num);
-			v.push_back(r);
-		}
-	}
-	return v;
-}
-
-vector<vector<int>> getSubsets_rec(int n,vector<vector<int>> &v){
-	if(n==1){
-		v.push_back({});
-		v.push_back({1});
-		return v;
-	}
-	else {
-		getSubsets_rec(n-1,v);
-		int l=v.size();
-		for(int i=0;i<l;i++){
-		//	cout<<"Adding partitions... for "<<i<<endl;
-			vector<int> r=v[i];
-			r.push_back(n);
-			v.push_back(r);
-		}
-	}
-	return v;
-}
-
-int main(){
-	int n;
-	cin>>n;
-	vector<vector<int>> v;
-	//v=getSubsets_rec(n,v);
-	//displaySubsets(v);
-	v.clear();
-	v=getSubsets_itr(n,v);
-	displaySubsets(v);
-	return 0;
-}
+class Solution {
+public:
+    void genSubsets(vector<int> nums, vector<int> output,vector<vector<int>> &res){
+            if(nums.size()==0){
+                res.push_back(output);
+                return;
+            }
+        
+            vector<int> v1;
+            vector<int> v2;
+            v1=v2=output;
+            
+            v2.push_back(nums[0]);
+            nums.erase(nums.begin()+0);
+            
+            genSubsets(nums,v1,res);
+            genSubsets(nums,v2,res);
+            return ;
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> output;
+        genSubsets(nums,output,res);
+        return res;
+    }
+};
